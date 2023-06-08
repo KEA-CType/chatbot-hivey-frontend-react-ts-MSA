540e2913e61f96c57f8f7c6bd4de0f19cb60adad
seonwoo <ska2870ghk@naver.com>
2023-06-08 16:54:57 +0900

    Fix: Adjust API response to MSA API

--- src/commons/interfaces/commonInterface.ts
+++ src/commons/interfaces/commonInterface.ts
@@ -49,11 +49,17 @@ export interface GroupMemberListResponse {
 export interface SubmissionListResponse {
     formId: number,
     memberId: number,
-    email: string,
-    name: string,
+    getUserRes: UserResponse,
     groupId: number,
     submit: boolean,
     mandatory: boolean
+}
+
+export interface UserResponse {
+    userId: number,
+    img: string,
+    email: string,
+    name: string,
 }
 
 export interface TargetGroupListResponse {
